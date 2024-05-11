8a9eb9d706a445a9d3bb0d2aaf7be61642a5ca46
micheleguidaa <michele.2017guida@gmail.com>
2024-05-11 09:01:15 +0200

    other updates

--- src/main/java/it/uniroma3/siw/controller/RicettaController.java
+++ src/main/java/it/uniroma3/siw/controller/RicettaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import it.uniroma3.siw.service.RicettaService;
 
@@ -17,4 +18,10 @@ public class RicettaController {
         model.addAttribute("ricette", ricettaService.findAll());
         return "ricette.html"; 
     }
+    
+	@GetMapping("/ricetta/{id}")
+	public String getRicetta(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("ricetta", this.ricettaService.findById(id));
+		return "ricetta.html";
+	}
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into ricetta (id,nome,descrizione) values(nextval('ricetta_seq'), 'Lasagna', 'Buona');
 insert into ricetta (id,nome,descrizione) values(nextval('ricetta_seq'), 'Cacio e Pepe', 'Deliziosa');
 insert into cuoco (id,nome,cognome,data_di_nascita,url_image) values(nextval('cuoco_seq'), 'Bruno', 'Barbieri','1962-01-12','/images/cuochi/Bruno-Barbieri.jpeg');
+INSERT INTO cuoco (id, nome, cognome, data_di_nascita, url_image) values (nextval('cuoco_seq'), 'Joe', 'Bastianich', '1968-09-17', '/images/cuochi/Joe-Bastianich.jpeg');
+insert into cuoco (id,nome,cognome,data_di_nascita,url_image) values (nextval('cuoco_seq'), 'Antonino', 'Cannavacciuolo', '1975-11-21', '/images/cuochi/Antonino-Cannavacciuolo.jpeg');
